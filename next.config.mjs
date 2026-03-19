9e446f98bb40123b6db06e990d3002e39981ed0b
Camila <camila.martinez@jazzya.com>
2026-03-19 20:44:41 -0300

    allow remote image domains

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,18 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
+  },
   allowedDevOrigins: ['192.168.1.89'],
 }
 
